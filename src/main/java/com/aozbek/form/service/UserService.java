0f731b342c89ccd41eb1875c6ee3a9eac87967ec
ahmetkaanozbek <ahmetkaanozbek@hotmail.com>
2023-12-24 18:10:50 +0300

    Added password encryption. Used BCrypt password encoder. Created a config package for that.

--- src/main/java/com/aozbek/form/service/UserService.java
+++ src/main/java/com/aozbek/form/service/UserService.java
@@ -3,17 +3,23 @@ package com.aozbek.form.service;
 import com.aozbek.form.model.User;
 import com.aozbek.form.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) { this.userRepository = userRepository; }
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public boolean createUser(User user) {
         if (!(userRepository.existsByUsername(user.getUsername()))) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
             userRepository.save(user);
             return true;
         }
